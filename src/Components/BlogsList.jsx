8818d5ebefb1461646a8fba99951898d2711ed20
Alec Down <alecdown@gmail.com>
2021-04-28 09:21:07 -0500

    adds search functionality inside of blogslist component'

--- src/Components/BlogsList.jsx
+++ src/Components/BlogsList.jsx
@@ -1,27 +1,55 @@
+import React, {useState} from 'react';
 import { makeStyles } from '@material-ui/core'
 import Grid from '@material-ui/core/Grid';
 import BlogCard from '../Components/BlogCard'
+import TextField from '@material-ui/core/TextField';
 
 const useStyles = makeStyles(() => ({
   root: {
     marginTop: "3%",
     padding: "2%"
+  },
+  searchContainer: {
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    paddingTop: 50
+  },
+  searchBox: {
+    width: 400,
+    maxWidth: '90%'
   }
 }))
 
 function BlogsList({ blogs, blogCardClickHandler }) {
   const classes = useStyles()
 
+  const [searchTerm, setSearchTerm] = useState('');
+
   function createBlogCards() {
-    return blogs.map(blogObject => (
+    return blogs.filter(blog => blog.title.toLowerCase().includes(searchTerm.toLowerCase())).map(blogObject => (
       <BlogCard key={blogObject.id} blogObject={blogObject} clickHandler={blogCardClickHandler} />
     ))
   }
+
   console.log(blogs)
+
   return (
+
+    <>
+    <div className={classes.searchContainer}>
+
+    <form  noValidate autoComplete="off">
+      <TextField className={classes.searchBox} id="standard-basic" label="Search..." onChange={e => setSearchTerm(e.target.value)} />
+    </form>
+
+    </div>
+
     <Grid className={classes.root} container spacing={3} >
       {createBlogCards()}
     </Grid >
+
+    </>
   )
 }
 
