7b3c1dc2a45fd29df3e1957cda4d9e565891b149
Alec Down <alecdown@gmail.com>
2021-04-28 11:39:38 -0500

    refactors app.js as a function component in prep for context

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Route } from 'react-router-dom'
 import './App.css'
 import BlogsContainer from './Containers/BlogsContainer'
 import Navbar from './Components/Navbar'
 
-class App extends React.Component{
-  state = {blogs: []}
+function App() {  
+    const [blogs, setBlogs] = useState([]);
 
-  async componentDidMount(){
+    useEffect(async () => {
+        // For demonstration purposes, we mock an API call.
         const resp = await fetch('http://localhost:5000/blogs')
-        const payload = await resp.json()
-        this.setState({blogs: payload})
-    }
-  
-  render(){
+        const payload = await resp.json();
+
+        setBlogs(payload);
+  }, []);
+
     return (
     <>
       <Navbar />
-      <Route path="/" render={()=> <BlogsContainer blogs={this.state.blogs}/>} />
+      <Route path="/" render={()=> <BlogsContainer blogs={blogs}/>} />
     </>
 
   )
   }
-  
-}
-
 
 export default App
